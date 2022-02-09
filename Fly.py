3645d8e9c6ed90542a8f0b6768c05f65af3e6284
Daniel Pedroza <Daniel_Pedroza1@baylor.edu>
2022-02-09 14:13:19 -0600

    finished Fly and started CellPhoneClass

--- Fly.py
+++ Fly.py
@@ -2,7 +2,8 @@ import InsectClass as i
 
 
 def main():
-    new_insect = i.Insect()
+    new_insect = i.Insect(2, 4)
+    new_insect.length_of_flight()
     miles = str(new_insect.get_lenflight())
     print("This insect can fly for " + miles + " miles.")
 
